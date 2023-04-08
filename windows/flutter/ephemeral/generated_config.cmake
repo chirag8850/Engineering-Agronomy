cc8d1c0827cae4070e8807429ef6ddb75c86dbb8
Chirag Vaviya <79510865+chirag8850@users.noreply.github.com>
2023-04-08 14:54:26 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\flutter_windows_3.7.1-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Lenovo\\OneDrive\\Desktop\\eng_agro\\login" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\flutter_windows_3.7.1-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\Lenovo\\OneDrive\\Desktop\\eng_agro\\login"
+  "FLUTTER_ROOT=D:\\flutter_windows_3.7.1-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Lenovo\\OneDrive\\Desktop\\eng_agro\\login\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Lenovo\\OneDrive\\Desktop\\eng_agro\\login"
+  "FLUTTER_TARGET=C:\\Users\\Lenovo\\OneDrive\\Desktop\\eng_agro\\login\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Lenovo\\OneDrive\\Desktop\\eng_agro\\login\\.dart_tool\\package_config.json"
+)
